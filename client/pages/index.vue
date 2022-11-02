586ca153d6d505f288f8ec9483f74fbf8a9eeb72
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-11-02 14:58:17 +0800

    Client: use Nuxt instead of vanilla Vue

---
+++ client/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>HI</p>
+    <NuxtLink to="/login">Login</NuxtLink>
+    <NuxtLink to="/test">Test</NuxtLink>
+  </div>
+</template>
---
+++ client/pages/test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>TEST</p>
+  </div>
+</template>
